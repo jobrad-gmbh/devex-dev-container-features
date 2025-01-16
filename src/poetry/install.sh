11372f85d396026b7210b9f4f5fec0b8ffb93e61
Urs Lange <urs.lange@jobrad.org>
2025-01-16 11:10:44 +0100

    Always install poetry to /opt/poetry

--- src/poetry/install.sh
+++ src/poetry/install.sh
@@ -6,4 +6,7 @@ apt-get update && apt-get install --no-install-recommends --yes curl
 
 # Install poerty
 curl -sSL https://install.python-poetry.org | python3 -
+
+# Fix PATH issues
+ln -snf $POETRY_HOME/bin/poetry /usr/local/bin/poetry
 echo "Activating feature 'poetry'"
